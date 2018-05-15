72b898fe7fa5a3ff6b687d41918c4a6455bce09e
Alex <wizardstudiokm@gmail.com>
2018-05-15 10:49:08 +0300

    fix

--- src/android/IntentSender.java
+++ src/android/IntentSender.java
@@ -1,16 +1,13 @@
 package com.rex.plugin;
 
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.tapapp.rex.MainActivity;
-
+import com.tapapp.rex.WorksService;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class IntentSender extends CordovaPlugin {
@@ -20,55 +17,21 @@ public class IntentSender extends CordovaPlugin {
 
         if (action.equals("intentSender")) {
 
-
-            for (int i = 0; i < data.length(); i++) {
-                Intent sendIntent = new Intent();
-                JSONObject jsonobject = data.getJSONObject(i);
-
-                if (jsonobject.has("putExtra")) {
-                    if (jsonobject.has("putExtraValue")) {
-                        sendIntent.putExtra(jsonobject.getString("putExtra"), jsonobject.getString("putExtraValue"));
-                    } else {
-                        sendIntent.putExtra(jsonobject.getString("putExtra"), "");
-                    }
+            ArrayList<String> list = new ArrayList<String>();
+            if (data != null) {
+                int len = data.length();
+                for (int i = 0; i < len; i++) {
+                    list.add(data.get(i).toString());
                 }
-                if (jsonobject.has("setAction")) {
-                    sendIntent.setAction(jsonobject.getString("setAction"));
-                }
-                if (jsonobject.has("setType")) {
-                    sendIntent.setType(jsonobject.getString("setType"));
-                }
-                if (jsonobject.has("setFlags")) {
-                    int flag;
-                    String flagData = jsonobject.getString("setFlags");
-                    switch (flagData) {
-                        case "FLAG_ACTIVITY_NEW_TASK":
-                            flag = Intent.FLAG_ACTIVITY_NEW_TASK;
-                            break;
-                        case "FLAG_ACTIVITY_SINGLE_TOP":
-                            flag = Intent.FLAG_ACTIVITY_SINGLE_TOP;
-                            break;
-                        case "FLAG_ACTIVITY_NO_HISTORY":
-                            flag = Intent.FLAG_ACTIVITY_NO_HISTORY;
-                            break;
-                        case "FLAG_ACTIVITY_MULTIPLE_TASK":
-                            flag = Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
-                            break;
-                        case "FLAG_ACTIVITY_CLEAR_TOP":
-                            flag = Intent.FLAG_ACTIVITY_CLEAR_TOP;
-                            break;
-                        default:
-                            flag = Intent.FLAG_ACTIVITY_NEW_TASK;
-                            break;
-                    }
-                    sendIntent.addFlags(flag);
-                }else {
-                    sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                }
-                cordova.getActivity().startActivity(sendIntent);
             }
 
-
+            if(list.get(0).equals("startService")) {
+                Intent sendIntent = new Intent(cordova.getActivity(), WorksService.class);
+                cordova.getActivity().startService(sendIntent);
+            }else{
+                Intent sendIntent = new Intent(cordova.getActivity(), WorksService.class);
+                cordova.getActivity().stopService(sendIntent);
+            }
             return true;
         } else
 
@@ -77,6 +40,5 @@ public class IntentSender extends CordovaPlugin {
             return false;
         }
     }
-
 
 }
